1bf3c794ec0d9e1a265c559aaa4d1683281642d9
shakerAbuDrais <thecoffeiner@gmail.com>
2022-12-10 19:04:07 +0300

    Add new tables and queries

--- schema.sql
+++ schema.sql
@@ -10,3 +10,23 @@ CREATE TABLE animals (
   neutered BOOLEAN,
   weight_kg DECIMAL
 );
+
+/* Project 3 step 1 */
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255) NOT NULL,
+  age INTEGER
+);
+
+/* section 1 step 2 */
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+/* section 1 step 3 */
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
