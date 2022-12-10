4d622c9dc24bc6e914a7f68942da66cdf1768d5b
shakerAbuDrais <thecoffeiner@gmail.com>
2022-12-10 14:11:14 +0300

    Add queries for the second project

--- queries.sql
+++ queries.sql
@@ -15,3 +15,56 @@ SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* second part of the project */
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * from animals;
+ROLLBACK;
+SELECT * from animals;
+ROLLBACK;
+SELECT * from animals;
+
+/* step 2 */
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * from animals;
+
+/* step 3 */
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * from animals;
+
+/* step 4 */
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+
+/* third part */
+
+SELECT COUNT(*) from animals;
+
+/* step 2 */
+SELECT COUNT(*) from animals WHERE escape_attempts = 0;
+
+/* step 3 */
+SELECT AVG(weight_kg) from animals;
+
+/* step 4 */
+SELECT neutered, MAX(escape_attempts) from animals GROUP BY neutered;
+
+/* step 5 */
+SELECT species, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY species;
+
+/* step 6 */
+SELECT species, AVG(escape_attempts) from animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
